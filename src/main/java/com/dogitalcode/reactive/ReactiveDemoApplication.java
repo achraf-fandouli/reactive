3224089cea6dd7bedf80ac4a28bdc1251fae0cee
achraf-fandouli <achref.fendouli@gmail.com>
2024-07-01 19:58:47 +0200

    Fix CommandLineRunner to insert data into the database.

--- src/main/java/com/dogitalcode/reactive/ReactiveDemoApplication.java
+++ src/main/java/com/dogitalcode/reactive/ReactiveDemoApplication.java
@@ -1,7 +1,11 @@
 package com.dogitalcode.reactive;
 
+import com.dogitalcode.reactive.model.Student;
+import com.dogitalcode.reactive.repository.StudentRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ReactiveDemoApplication {
@@ -10,4 +14,18 @@ public class ReactiveDemoApplication {
 		SpringApplication.run(ReactiveDemoApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(StudentRepository repository) {
+		return args -> {
+			for (int i = 0; i < 3000; i++) {
+				repository.save(
+						Student.builder()
+								.firstname("Achraf" + i)
+								.lastname("Fandouli" + i)
+								.age(i)
+								.build()
+				).subscribe();
+			}
+		};
+	}
 }
